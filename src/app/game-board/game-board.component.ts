864380b9c9cd6fe2a108609ffda2a43499d9cf17
toua moua <touam@msn.com>
2025-03-28 10:24:39 -0600

    added method to check for winners

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface MyTable {
+  [key: string]: string;
+}
+
 @Component({
   selector: 'app-game-board',
   standalone: true,
@@ -8,9 +12,10 @@ import { Component } from '@angular/core';
   styleUrl: './game-board.component.less'
 })
 export class GameBoardComponent {
-  public player = 'X';
-  public nextPlayer = 'O';
-  public winner = 'None';
+  public player: string = 'X';
+  public nextPlayer: string = 'O';
+  public winner: string = 'None';
+  private myTable: MyTable = {};
 
   public onItemClick(element: HTMLButtonElement) {
     // do nothing if square is already used.
@@ -22,19 +27,84 @@ export class GameBoardComponent {
     // set square to current player (i.e. X or O).
     element.innerText = this.player;
 
+    // update the game table.
+    this.myTable[element.id] = this.player;
+
     // update current player to next player.
-    let temp = this.player;
+    let temp: string = this.player;
     this.player = this.nextPlayer;
     this.nextPlayer = temp;
+
+    // check for winner.
+    this.checkGame();
   }
 
   public reset() {
-    let squares = document.querySelectorAll('button.square');
+    let squares: NodeListOf<Element> = document.querySelectorAll('button.square');
     squares.forEach((square) => (square as HTMLButtonElement).innerText = '');
 
     this.player = 'X';
     this.nextPlayer = 'O';
     this.winner = 'None';
+    this.myTable = {};
+  }
+
+  private checkGame () {
+    // top row wins.
+    if (
+      this.myTable['1:1'] !== undefined && this.myTable['1:1'] === this.myTable['1:2'] && this.myTable['1:2'] === this.myTable['1:3']
+    ) {
+      this.winner = this.myTable['1:1'] + ' wins top row!';
+    }
+
+    // middle row wins.
+    if (
+      this.myTable['2:1'] !== undefined && this.myTable['2:1'] === this.myTable['2:2'] && this.myTable['2:2'] === this.myTable['2:3']
+    ) {
+      this.winner = this.myTable['2:1'] + ' wins middle row!';
+    }
+
+    // bottom row wins.
+    if (
+      this.myTable['3:1'] !== undefined && this.myTable['3:1'] === this.myTable['3:2'] && this.myTable['3:2'] === this.myTable['3:3']
+    ) {
+      this.winner = this.myTable['3:1'] + ' wins bottom row!';
+    }
+
+    // first column wins.
+    if (
+      this.myTable['1:1'] !== undefined && this.myTable['1:1'] === this.myTable['2:1'] && this.myTable['2:1'] === this.myTable['3:1']
+    ) {
+      this.winner = this.myTable['1:1'] + ' wins first column!';
+    }
+
+    // second column wins.
+    if (
+      this.myTable['1:2'] !== undefined && this.myTable['1:2'] === this.myTable['2:2'] && this.myTable['2:2'] === this.myTable['3:2']
+    ) {
+      this.winner = this.myTable['1:2'] + ' wins second column!';
+    }
+
+    // third column wins.
+    if (
+      this.myTable['1:3'] !== undefined && this.myTable['1:3'] === this.myTable['2:3'] && this.myTable['2:3'] === this.myTable['3:3']
+    ) {
+      this.winner = this.myTable['1:3'] + ' wins third column!';
+    }
+
+    // reverse diagonal wins.
+    if (
+      this.myTable['1:1'] !== undefined && this.myTable['1:1'] === this.myTable['2:2'] && this.myTable['2:2'] === this.myTable['3:3']
+    ) {
+      this.winner = this.myTable['1:1'] + ' wins diagonal!';
+    }
+
+    // forward diagonal wins.
+    if (
+      this.myTable['1:3'] !== undefined && this.myTable['1:3'] === this.myTable['2:2'] && this.myTable['2:2'] === this.myTable['3:1']
+    ) {
+      this.winner = this.myTable['1:3'] + ' wins diagonal!';
+    }
   }
 
   /**
