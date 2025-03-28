95522030271fb6e8e18bc825ffc3c69bc9e61e9b
toua moua <touam@msn.com>
2025-03-27 20:18:03 -0600

    added reset functionality

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -9,21 +9,38 @@ import { Component } from '@angular/core';
 })
 export class GameBoardComponent {
   public player = 'X';
-  public nextPlayer = "O";
-  public winner = "None"
+  public nextPlayer = 'O';
+  public winner = 'None';
 
-  onItemClick(element: HTMLButtonElement) {
+  public onItemClick(element: HTMLButtonElement) {
+    // do nothing if square is already used.
     if (element.innerText !== '') {
       console.log('returned');
       return;
     }
 
-    // set square to current player (i.e. X or O) 
+    // set square to current player (i.e. X or O).
     element.innerText = this.player;
 
-    // update current player to next player
+    // update current player to next player.
     let temp = this.player;
     this.player = this.nextPlayer;
     this.nextPlayer = temp;
   }
+
+  public reset() {
+    let squares = document.querySelectorAll('button.square');
+    squares.forEach((square) => (square as HTMLButtonElement).innerText = '');
+
+    this.player = 'X';
+    this.nextPlayer = 'O';
+    this.winner = 'None';
+  }
+
+  /**
+   * TODO:
+   *  - Reset button needs to clear game board.
+   *  - Check game status after each player goes.
+   *  - Display winner.
+   */
 }
